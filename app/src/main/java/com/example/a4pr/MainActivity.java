7678b8a857a72bcf09f0fcf183b3a34eb885c90f
Anton Larionov <payne3105@gmail.com>
2022-03-24 11:38:47 +0300

    init

---
+++ app/src/main/java/com/example/a4pr/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.a4pr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+    }
+
+    public boolean onCreateActionMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+}
