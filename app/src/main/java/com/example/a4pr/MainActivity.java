f29dd510ac710da7366a51033cc679d89fdb989f
payne3105 <payne3105@gmail.com>
2022-03-25 14:12:09 +0300

    version 0.1 - added menu - added EditText - added button - menu is now working

--- app/src/main/java/com/example/a4pr/MainActivity.java
+++ app/src/main/java/com/example/a4pr/MainActivity.java
@@ -2,25 +2,34 @@ package com.example.a4pr;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
 
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+    View searchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        findId();
     }
 
-    public boolean onCreateActionMenu(Menu menu) {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.main_activity_actions, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
+    public void findId() {
+        searchButton = findViewById(R.id.action_search);
+    }
 }
