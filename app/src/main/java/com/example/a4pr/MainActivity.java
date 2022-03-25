99dd2a24b2b69973d912468190c833287509df0d
payne3105 <payne3105@gmail.com>
2022-03-25 18:09:28 +0300

    version 1.0 - fully added functionality

--- app/src/main/java/com/example/a4pr/MainActivity.java
+++ app/src/main/java/com/example/a4pr/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.a4pr;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.ClipData;
 import android.os.Bundle;
 import android.view.Menu;
@@ -9,6 +10,7 @@ import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.Objects;
 
@@ -16,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
     Button searchButton;
     EditText editText;
+    TextView insertText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,17 +37,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void findId() {
-        searchButton = (Button) findViewById(R.id.action_search);
+        searchButton = (Button) findViewById(R.id.buttonSend);
         editText = (EditText) findViewById(R.id.textEdit);
+        insertText = (TextView) findViewById(R.id.insertText);
     }
 
 
     public void messageSend() {
         searchButton.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View view) {
-                String inputText = editText.getText().toString().trim();
-
+                String inputText = editText.getText().toString();
+                insertText.setText(getResources().getString(R.string.greetings) + " " + inputText);
             }
         });
     }
