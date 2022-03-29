68aa2aec063c0bb3d254e3e53dc86ff2aa1fbc0b
payne3105 <payne3105@gmail.com>
2022-03-29 21:54:18 +0300

    version 2.0 - fully ended tasks

--- app/src/main/java/com/example/a4pr/MainActivity.java
+++ app/src/main/java/com/example/a4pr/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.a4pr;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.ClipData;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -16,17 +18,57 @@ import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button searchButton;
+    Button sendButton;
     EditText editText;
     TextView insertText;
+    TextView sdkText;
+    String inputText;
 
     @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        findId();
+        sdkText.setText("");
+    }
+
+    @Override
+    @SuppressLint("SetTextI18n")
+    protected void onResume() {
+        super.onResume();
+
+        String sdk = String.valueOf(Build.VERSION.SDK_INT);
+        sdkText.setText(sdk + " SDK");
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putString(inputText, editText.getText().toString());
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    @SuppressLint("SetTextI18n")
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         findId();
         messageSend();
+
+        if (savedInstanceState != null) {
+            insertText.setText(getResources().getString(R.string.greetings) + " " + savedInstanceState.getString(inputText));
+        }
     }
 
     @Override
@@ -37,18 +79,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void findId() {
-        searchButton = (Button) findViewById(R.id.buttonSend);
+        sendButton = (Button) findViewById(R.id.buttonSend);
         editText = (EditText) findViewById(R.id.textEdit);
         insertText = (TextView) findViewById(R.id.insertText);
+        sdkText = (TextView) findViewById(R.id.sdkText);
     }
 
 
     public void messageSend() {
-        searchButton.setOnClickListener(new View.OnClickListener() {
+        sendButton.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View view) {
-                String inputText = editText.getText().toString();
+                inputText = editText.getText().toString();
                 insertText.setText(getResources().getString(R.string.greetings) + " " + inputText);
             }
         });
